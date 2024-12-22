56e739dbc601ef2c8fc09570019469cf4ff65a77
Ben Borgers <borgersbenjamin@gmail.com>
2024-12-22 10:37:37 -0500

    Create button component (#6)

---
+++ app/components/button.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+
+export default function Button({
+  text,
+  route,
+  variant,
+  icon = null,
+}: {
+  text: string;
+  route: string;
+  variant: "primary" | "secondary" | "ghost";
+  icon?: React.ElementType;
+}) {
+  const Icon = icon;
+  return (
+    <a
+      href={route}
+      className={clsx(
+        "block max-w-max px-4 py-2 rounded-lg font-semibold",
+        "flex items-center gap-2",
+        "hover:ring-2 hover:ring-offset-2 hover:ring-offset-black transition-shadow duration-200",
+        variant === "primary" && "bg-brand text-gray-900 hover:ring-brand",
+        variant === "secondary" && "bg-white text-gray-900 hover:ring-white",
+        variant === "ghost" && "text-white hover:ring-white"
+      )}
+    >
+      {icon && <Icon className="w-4 h-4" />}
+      <span>{text}</span>
+    </a>
+  );
+}
