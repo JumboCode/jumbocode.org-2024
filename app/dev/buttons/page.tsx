61633c6b3116cfd2f17626947b261456e86d4608
Ben Borgers <borgersbenjamin@gmail.com>
2025-01-01 17:30:34 -0500

    Move buttons demo page to `/dev/buttons` (#7)

---
+++ app/dev/buttons/page.tsx
@@ -0,0 +1,31 @@
+import Button from "@/components/button";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+
+export default function Home() {
+  return (
+    <div className="p-4 space-y-4 bg-black">
+      <Button
+        text="Email Us"
+        route="/testing"
+        variant="primary"
+        icon={EnvelopeIcon}
+      />
+
+      <Button
+        text="Email Us"
+        route="/testing"
+        variant="secondary"
+        icon={EnvelopeIcon}
+      />
+
+      <Button
+        text="Email Us"
+        route="/testing"
+        variant="ghost"
+        icon={EnvelopeIcon}
+      />
+
+      <Button text="Email Us" route="/testing" variant="primary" />
+    </div>
+  );
+}
