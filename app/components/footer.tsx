a309312292ae0af7122eee18ca117f61d7c16386
hkittelberger <holdenkittelberger@gmail.com>
2025-01-02 16:05:30 -0500

    hello world

---
+++ app/components/footer.tsx
@@ -0,0 +1,12 @@
+import clsx from "clsx";
+
+export default function Footer() {
+  
+  
+  
+    return (
+    <footer>
+      <p>JumboCode is da best</p>
+    </footer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,36 @@
 import Button from "@/app/components/button";
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import Footer from "@/app/components/footer";
 
 export default function Home() {
   return (
-    <div className="p-4 space-y-4 bg-black">
-      <Button
-        text="Email Us"
-        route="/testing"
-        variant="primary"
-        icon={EnvelopeIcon}
-      />
+    <div>
+      <div className="p-4 space-y-4 bg-black">
+        <Button
+          text="Email Us"
+          route="/testing"
+          variant="primary"
+          icon={EnvelopeIcon}
+        />
 
-      <Button
-        text="Email Us"
-        route="/testing"
-        variant="secondary"
-        icon={EnvelopeIcon}
-      />
+        <Button
+          text="Email Us"
+          route="/testing"
+          variant="secondary"
+          icon={EnvelopeIcon}
+        />
 
-      <Button
-        text="Email Us"
-        route="/testing"
-        variant="ghost"
-        icon={EnvelopeIcon}
-      />
+        <Button
+          text="Email Us"
+          route="/testing"
+          variant="ghost"
+          icon={EnvelopeIcon}
+        />
 
-      <Button text="Email Us" route="/testing" variant="primary" />
+        <Button text="Email Us" route="/testing" variant="primary" />
+
+      </div>
+      <Footer/>
     </div>
   );
 }
