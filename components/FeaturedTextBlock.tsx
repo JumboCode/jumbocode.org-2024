e40bdd4f1ae4a19e84796e40dd784d8f6f54dbf3
Gabriel Sessions <gsess7732@gmail.com>
2025-01-03 17:56:28 -0500

    FeaturedTextBlock component

---
+++ components/FeaturedTextBlock.tsx
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview A simple section with a small title and a block of bolded text
+ * 
+ * @file FeaturedTextBlock.tsx
+ * @date January 3rd, 2025
+ * @author Gabriel Sessions
+ * 
+ */
+
+import { FC } from "react"
+
+interface FeaturedTextBlockProps {
+  title: FC,
+  featuredText: FC
+}
+
+/**
+ * 
+ */
+export default function FeaturedTextBlock(props: FeaturedTextBlockProps) {
+  return (
+    <>
+      <div className="flex py-10 px-6 md:py-10 md:px-12 lg:py-28 lg:px-28 flex-col justify-center items-start bg-gray gap-2">
+        <h2 className="text-md md:text-lg lg:text-xl text-white opacity-60 font-bold">{props.title}</h2>
+        <h3 className="text-xl md:text-3xl lg:text-5xl text-white font-semibold lg:leading-[56px]">{props.featuredText}</h3>
+      </div>
+    </>
+  )
+}
