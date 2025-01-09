651ddad638611a70e279b57daac120a03847b617
myix765 <myix765@gmail.com>
2025-01-09 03:16:43 -0500

    created collage using margins to position

---
+++ app/components/OurPartnerships.tsx
@@ -0,0 +1,54 @@
+
+
+export default function OurPartnerships() {
+    return (
+        <div className="flex flex-col items-center gap-y-12">
+            <h2 className="text-2xl font-semibold text-white">Our Partnerships</h2>
+
+            <div className="w-full flex flex-wrap justify-between bg-red-700 p-4">
+                <div className="mt-1">
+                    <img src="/partnerships/Peace_corps_logo.png" alt="Peace Corps" className="object-contain" />
+                </div>
+                <div>
+                    <img src="/partnerships/HomeStart_logo.png" alt="HomeStart" className="object-contain" />
+                </div>
+                <div className="mt-3">
+                    <img src="/partnerships/Bergen_Family_Center_logo.png" alt="Bergen Family Center" className="object-contain" />
+                </div>
+                <div className="ml-16">
+                    <img src="/partnerships/SS_Villages_logo.png" alt="SimplySmiles children's villages" className="object-contain" />
+                </div>
+                <div className="mt-3">
+                    <img src="/partnerships/Bisexual_Resource_Center_logo.png" alt="Bisexual Resource Center" className="object-contain" />
+                </div>
+                <div className="mt-3 ml-4">
+                    <img src="/partnerships/Casa_Myrna_logo.png" alt="Casa Myrna" className="object-contain" />
+                </div>
+                <div className="-mt-12">
+                    <img src="/partnerships/Animal_Aid_Logo.png" alt="Animal Aid" className="object-contain" />
+                </div>
+                <div className="-mt-16 ml-8">
+                    <img src="/partnerships/kmb_logo.png" alt="Keep Massachusetts Beautiful" className="object-contain" />
+                </div>
+                <div className="mt-4">
+                    <img src="/partnerships/Dress_for_Success_logo.png" alt="Dress For Success" className="object-contain" />
+                </div>
+                <div className="-mb-5 ml-20 mr-14">
+                    <img src="/partnerships/Ocean_Alliance_Logo.png" alt="Ocean Alliance" className="object-contain" />
+                </div>
+                <div className="-mt-8">
+                    <img src="/partnerships/AaronsPresents_logo.png" alt="Aaron's Presents" className="object-contain" />
+                </div>
+                <div className="-mt-8">
+                    <img src="/partnerships/images-4.png" alt="" className="object-contain" /> {/* not sure whose logo this is */}
+                </div>
+                <div className="-mt-16">
+                    <img src="/partnerships/potencialogo.png" alt="Potencia" className="object-contain" />
+                </div>
+                <div className="mr-[40rem] -mt-16">
+                    <img src="/partnerships/Emerald_Necklace_Conservancy_logo.png" alt="Emerald Necklace Conservancy" className="object-contain" />
+                </div>
+            </div>
+        </div >
+    )
+}
---
+++ app/dev/image-collages/page.tsx
@@ -0,0 +1,9 @@
+import OurPartnerships from "@/app/components/OurPartnerships";
+
+export default function ImageCollages() {
+    return (
+        <div className="bg-gray-950 min-h-screen px-[7.5rem] py-24">
+            <OurPartnerships />
+        </div>
+    )
+}
