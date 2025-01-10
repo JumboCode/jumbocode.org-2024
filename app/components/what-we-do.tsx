a892f6987f8fc0137d64c91e1bd91d411c29a2aa
Ben Borgers <borgersbenjamin@gmail.com>
2025-01-09 20:18:58 -0500

    Switch copy

--- app/components/what-we-do.tsx
+++ app/components/what-we-do.tsx
@@ -18,19 +18,26 @@ export default function WhatWeDo({
         <Card
           title="🗓️ Year-Long Projects With NPOs"
           body={
-            <>
+            variant === "compact" ? (
               <p>
-                Teams complete software development projects for{" "}
-                <strong>Boston area non-profits</strong> under the guidance of a
-                Project Manager, Tech Lead, and Designer.
+                Teams of 12 students work with non-profit organizations to
+                design, develop, and ship a digital solution.
               </p>
-              <p>
-                Students gain{" "}
-                <strong>hands-on experience building web applications</strong>{" "}
-                from the ground up while working closely with clients to meet
-                their specific needs.
-              </p>
-            </>
+            ) : (
+              <>
+                <p>
+                  Teams complete software development projects for{" "}
+                  <strong>Boston area non-profits</strong> under the guidance of
+                  a Project Manager, Tech Lead, and Designer.
+                </p>
+                <p>
+                  Students gain{" "}
+                  <strong>hands-on experience building web applications</strong>{" "}
+                  from the ground up while working closely with clients to meet
+                  their specific needs.
+                </p>
+              </>
+            )
           }
           imageSrc="/culture-house.jpg"
         />
@@ -38,17 +45,24 @@ export default function WhatWeDo({
         <Card
           title="🧑‍🤝‍🧑 Team Collaboration"
           body={
-            <>
+            variant === "compact" ? (
               <p>
-                JumboCode fosters collaboration among students of{" "}
-                <strong>all majors, academic years, and skill levels.</strong>
+                JumboCode creates opportunities for students with varying
+                majors, ages, and skills to connect with one another.
               </p>
-              <p>
-                Teams work together to exchange ideas, strengthen their skills,
-                and <strong>tackle real-world challenges</strong> in a
-                supportive learning environment.
-              </p>
-            </>
+            ) : (
+              <>
+                <p>
+                  JumboCode fosters collaboration among students of{" "}
+                  <strong>all majors, academic years, and skill levels.</strong>
+                </p>
+                <p>
+                  Teams work together to exchange ideas, strengthen their
+                  skills, and <strong>tackle real-world challenges</strong> in a
+                  supportive learning environment.
+                </p>
+              </>
+            )
           }
           imageSrc="/my-lingual-lawyer.jpg"
         />
@@ -56,20 +70,28 @@ export default function WhatWeDo({
         <Card
           title="🧩 Community Events"
           body={
-            <>
+            variant === "compact" ? (
               <p>
-                We host events like field day, guest speakers, project
-                showcases, and a weekly Hack Night to{" "}
-                <strong>
-                  connect members and celebrate their achievements
-                </strong>
-                .
+                We arrange events like field day and project presentations,
+                allowing our members to bond with each other and the Tufts
+                community.
               </p>
-              <p>
-                JumboCode events serve members and the broader Tufts community,
-                uniting students across disciplines.
-              </p>
-            </>
+            ) : (
+              <>
+                <p>
+                  We host events like field day, guest speakers, project
+                  showcases, and a weekly Hack Night to{" "}
+                  <strong>
+                    connect members and celebrate their achievements
+                  </strong>
+                  .
+                </p>
+                <p>
+                  JumboCode events serve members and the broader Tufts
+                  community, uniting students across disciplines.
+                </p>
+              </>
+            )
           }
           imageSrc="/jumbohack-crowd.jpg"
         />
@@ -77,18 +99,26 @@ export default function WhatWeDo({
         <Card
           title="🏆 JumboHack"
           body={
-            <>
+            variant === "compact" ? (
               <p>
-                Each year we host JumboHack, a{" "}
-                <strong>48-hour hackathon</strong> that brings together Boston
-                area students to develop software for social good.{" "}
+                Each year we host JumboHack, a 48-hour hackathon that brings
+                together Boston area students to develop software for social
+                good.
               </p>
-              <p>
-                It’s a fast-paced, collaborative event designed to inspire
-                creativity and drive impact. To learn more, visit the{" "}
-                <a href="https://jumbohack.org">JumboHack website</a>.
-              </p>
-            </>
+            ) : (
+              <>
+                <p>
+                  Each year we host JumboHack, a{" "}
+                  <strong>48-hour hackathon</strong> that brings together Boston
+                  area students to develop software for social good.{" "}
+                </p>
+                <p>
+                  It's a fast-paced, collaborative event designed to inspire
+                  creativity and drive impact. To learn more, visit the{" "}
+                  <a href="https://jumbohack.org">JumboHack website</a>.
+                </p>
+              </>
+            )
           }
           imageSrc="/jumbohack-checkin.jpg"
         />
