b8845fc305646853131c20c133ecf4f9b49e738e
Gabriel Sessions <57270441+gabrielsessions@users.noreply.github.com>
2025-02-04 13:44:05 -0500

    Gabe/project timeline (#25)

---
+++ app/dev/project-timeline/page.tsx
@@ -0,0 +1,11 @@
+import ProjectTimeline from "@/components/ProjectTimeline";
+
+
+export default function ProjectTimelinePage() {
+
+  return (
+    <>
+      <ProjectTimeline className="my-24" />
+    </>
+  )
+}
