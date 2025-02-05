0abfb8df7195229a71b844c930a269ebd7e792af
Holden Kittelberger <115906014+hkittelberger@users.noreply.github.com>
2025-02-05 00:17:59 -0500

    Member Career Paths + Our Partnerships (#23)
    Co-authored-by: myix765 <myix765@gmail.com>

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/hero";
+import OurPartnerships from "@/components/OurPartnerships";
 
 export default function AboutPage() {
   return (
@@ -13,6 +14,7 @@ export default function AboutPage() {
           </>
         }
       />
+      <OurPartnerships />
     </div>
   );
 }
