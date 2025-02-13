7f8ce16bc632aa6a165996271ca93cf3163bc241
Gabriel Sessions <gsess7732@gmail.com>
2025-02-13 00:13:16 -0500

    Project-specific page: HomeStart

---
+++ components/Projects/ProjectPage.tsx
@@ -0,0 +1,126 @@
+import clsx from "clsx"
+import Image from "next/image"
+import React from "react"
+import Hero from "../hero"
+import { ImageProps } from "../ImageCarousel"
+
+// Layout of a JumboCode project page in JSON format
+export interface ProjectPageProps {
+  hero: {
+    projectName: string,
+    schoolYear: string,
+    image: ImageProps
+  },
+  overview: {
+    logo: ImageProps,
+    summary: string,
+    projectGoal: string,
+    teamMembers: {
+      leadership: string,
+      developers: string
+    },
+    teamPicture: ImageProps
+  },
+  techStack: Array<{
+    name: string,
+    logo: ImageProps
+  }>,
+  finalScreens: Array<ImageProps>
+}
+
+const h3Class = "text-white text-3xl font-semibold";
+
+export default function ProjectPage(props: ProjectPageProps) {
+  return (
+    <>
+      <Hero
+        title={<>{props.hero.projectName}</>}
+        subtitle={<>{props.hero.schoolYear}</>}
+        image={
+          <Image
+            src={props.hero.image.src}
+            alt={props.hero.image.alt}
+            width={800}
+            height={400}
+          />
+        }
+      />
+
+      <div className="md:grid md:grid-cols-2">
+        <div>
+          <TextSection
+            title={props.hero.projectName}
+            text={<p>{props.overview.summary}</p>}
+          />
+          <TextSection
+            title="Project Goal"
+            text={<p>{props.overview.projectGoal}</p>}
+          />
+        </div>
+
+        <div className="flex justify-center items-center mx-6 mb-12 md:mb-0">
+          <Image
+            src={props.overview.teamPicture.src}
+            alt={props.overview.teamPicture.alt}
+            width={465}
+            height={295}
+            className="border-transparent rounded-xl"
+          />
+        </div>
+
+      </div>
+      <TextSection
+        title="Team Members"
+        text={
+          <>
+            <p className="mb-4"><span className="font-bold">Leadership: </span>
+              {props.overview.teamMembers.leadership}</p>
+
+            <p><span className="font-bold">Developers: </span>
+              {props.overview.teamMembers.developers}</p>
+          </>
+        }
+      />
+
+      <h3 className={clsx(h3Class, "mb-12")}>Tech Stack</h3>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:flex md:text-center md:space-y-0">
+        {
+          props.techStack.map((elem, idx) => {
+            return (
+              <div key={idx} className="m-auto text-center mb-8">
+                <div className="h-[55px] flex items-center justify-center">
+                  <Image
+                    src={elem.logo.src}
+                    alt={elem.logo.alt}
+                    width={55}
+                    height={55}
+                  />
+                </div>
+                <p className="text-white mt-4">{elem.name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+
+      <h3 className={clsx(h3Class, "mb-12")}>Final Screens</h3>
+    </>
+  )
+}
+
+
+
+
+interface TextSectionProps {
+  title: string,
+  text: React.ReactNode,
+  logo?: ImageProps
+}
+function TextSection(props: TextSectionProps) {
+  return (
+    <div>
+      <h3 className={h3Class}>{props.title}</h3>
+      <div className="text-gray-200 my-4 mb-12">{props.text}</div>
+    </div>
+  )
+}
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,21 +1,33 @@
+import clsx from "clsx";
+
+// Images + Buttons probably don't work - Gabe (Feb 2025) 
 export default function Hero({
   title,
   subtitle,
   buttons,
+  image
 }: {
   title: React.ReactNode;
   subtitle: React.ReactNode;
   buttons?: React.ReactNode;
+  image?: React.ReactNode;
 }) {
   return (
-    <div className="py-24 sm:py-48">
-      <h1 className="text-white font-semibold text-4xl">{title}</h1>
+    <div className={clsx(
+      image && "md:flex md:space-x-64",
+      "py-24 sm:py-48"
+    )}>
+      <div>
+        <h1 className="text-white font-semibold text-4xl">{title}</h1>
 
-      <h2 className="mt-4 text-white/70 text-xl font-medium max-w-screen-md">
-        {subtitle}
-      </h2>
+        <h2 className="mt-4 text-white/70 text-xl font-medium max-w-screen-md">
+          {subtitle}
+        </h2>
+      </div>
 
       {buttons && <div className="mt-8 flex gap-x-4">{buttons}</div>}
+
+      {image && <div className="-mt-24">{image}</div>}
     </div>
   );
 }
