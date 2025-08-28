d1bc94054bfb3fd03be80c66ab8efc0ad528f44b
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-08-28 15:40:33 -0400

    Add more padding to mobile

--- app/(isolated)/layout.tsx
+++ app/(isolated)/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <Analytics/>
       <body className="antialiased bg-gray-950">
-        <div className="max-w-screen-lg mx-auto px-4 pt-4 sm:pt-8 pb-24">
+        <div className="max-w-screen-lg mx-auto px-8 pt-4 sm:pt-8 pb-24">
           {/* Gradient background */}
           <div className="absolute inset-0 h-[85vh] bg-gradient-to-b from-brand/25 to-gray-950 -z-10" />
 
