7d94e5a8b27953895ef3575697f0151d81bb7654
hkittelberger <holdenkittelberger@gmail.com>
2025-12-21 15:44:12 -0500

    added new subpage category jumbocup

---
+++ app/(jumbocup)/jumbocup/page.tsx
@@ -0,0 +1,17 @@
+/*  page.tsx
+*
+*   Page for viewing the current Jumbocup leaderboard
+* 
+*   Created by Holden Kittelberger on 11/29/2025
+*
+*/
+
+
+
+export default function JumboCupPage() {
+    return (
+        <div className="flex flex-col items-center justify-center mt-8">
+            <h1 className="text-2xl font-bold">Current Jumbocup Leaderboard</h1>
+        </div>   
+    );
+}
