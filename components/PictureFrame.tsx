a69e274a2240c3ac9a97548460ccaaf098075044
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-03-30 22:56:57 -0400

    Feat: if no image, only display name & role

--- components/PictureFrame.tsx
+++ components/PictureFrame.tsx
@@ -2,12 +2,15 @@
 *
 *   This file creates a picture frame component. It takes in a picture, name,
 *   and description as props and displays them in a frame.
-* 
+*
 *   Created by Holden Kittelberger on 2/5/2025
 *
 */
 
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 type Frame = {
     src: string;
@@ -16,9 +19,13 @@ type Frame = {
 }
 
 const PictureFrame: React.FC<Frame> = ({ src, name, description }) => {
+    const [imgError, setImgError] = useState(false);
+
     return (
         <div className="flex flex-col border border-off_white p-3 rounded-lg lg:p-4 sm:text-sm">
-            <Image src={src} alt={name} width={300} height={300} className="object-cover w-full rounded-lg mb-3 lg:mb-5" />
+            {!imgError && (
+                <Image src={src} alt={name} width={300} height={300} className="object-cover w-full rounded-lg mb-3 lg:mb-5" onError={() => setImgError(true)} />
+            )}
             <h2 className="text-sm md:text-lg lg:text-xl font-bold text-white mb-2 sm:mb-1">
             {name}
             </h2>
